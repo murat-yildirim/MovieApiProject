7040171138086954509de743818ec34ee2ba9878
Murat Yıldırım <30393559+murat-yildirim@users.noreply.github.com>
2025-02-07 23:52:14 +0300

    4.Ders sonu Entity'lerin tamamlanması ve Context sınıfının oluşturulması

---
+++ Infrastructure/MovieApiProject.Persistence/Context/MovieContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApiProject.Persistence.Context
+{
+    public class MovieContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-363RU31\\SQLEXPRESS;initial Catalog=ApiMovieDb;integrated security=true");
+        }
+
+        public DbSet<Category> Categorys { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Cast> Casts { get; set; }
+    }
+}
