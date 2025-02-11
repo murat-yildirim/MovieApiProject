c4e02fac3fe0cf8b38adf3b295530345cc2460a8
Murat Yıldırım <30393559+murat-yildirim@users.noreply.github.com>
2025-02-11 22:55:38 +0300

    6-7. Ders CQRS tasarım deseni kullanımı için dosya tanımlamaları yapıldı ve classları oluşturuldu Commands,Queries,Results

---
+++ Core/MovieApiProject.Application/Features/CQRSDesignPattern/Commands/CategoryCommands/UpdateCategoryCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApiProject.Application.Features.CQRSDesignPattern.Commands.CategoryCommands
+{
+    public class UpdateCategoryCommand
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+
+
+    }
+}
